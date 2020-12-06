85b96b92f9a97f73e8a2624e332c4dd41eaa81cc
Caroline Courtney <caroline@carolinecourtney.com>
2020-12-06 16:16:25 -0500

    basic components for a form, in preparation for Firebase section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import Header from './components/header/header';
 import HomePage from './pages/homepage/homepage'
 import ShopPage from './pages/shoppage/shoppage';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
 
 const HatsPage = () => (
   <div>
@@ -18,6 +19,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
